db78d970e7fb23b36e77674f58662f05a3f77056
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-01 18:00:48 -0500

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      backgroundColor: const Color.fromRGBO(25, 27, 31, 1),
+
+    );
+  }
+
+
+
+}
