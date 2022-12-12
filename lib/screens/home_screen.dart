973b86eeb728aabf61083481ffc2a3ab895346c4
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-12 04:33:32 -0500

    Created the "appbar" of the home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,14 +5,46 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-      backgroundColor: const Color.fromRGBO(25, 27, 31, 1),
-
+    return SafeArea(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+              iconSize: 30.0,
+              color: Colors.white,
+              icon: const Icon(Icons.account_circle_outlined),
+              tooltip: "User",
+              onPressed: () {},
+            ),
+            const Text("Home", style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+              fontFamily: "roboto"
+            ), ),
+            IconButton(
+              color: Colors.white,
+              iconSize: 30.0,
+              icon: const Icon(Icons.settings_outlined),
+              tooltip: "Configuration",
+              onPressed: () {}),
+            ],
+            ),
+          ),
+          Expanded(child: Container())
+          ],
+        ),
     );
   }
 
 
 
 }
+
+
