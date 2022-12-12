2404929dfc0ed0b9140138b3de9eb69026f5ce11
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-12 04:33:49 -0500

    Exaple pages that will be changed later

---
+++ lib/screens/pag2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pag2 extends StatelessWidget {
+  const Pag2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("2", selectionColor: Colors.black,));
+  }
+}
---
+++ lib/screens/pag3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pag3 extends StatelessWidget {
+  const Pag3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("3", selectionColor: Colors.black,));
+  }
+}
