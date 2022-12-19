357d2cabcd7a0827f0215d2fffb73749cb8b74e4
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-19 14:43:07 -0500

    Added a new color white with less contrast

--- lib/palette/palette.dart
+++ lib/palette/palette.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class PALETTE{
+  static const Color blanco = Color.fromARGB(227, 246, 246, 246);
   static const Color backgroud = Color.fromRGBO(23, 27, 31, 1);
   static const Color secundary = Color.fromRGBO(28, 30, 34, 1);
   static const Color unSelected = Color.fromRGBO(153,156,165,1);
+
 }
