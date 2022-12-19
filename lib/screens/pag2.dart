fbc6c5a84c76884c698a590de7bcb1ef1ade23b1
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-19 16:29:36 -0500

    modifing example pages

--- lib/screens/pag2.dart
+++ lib/screens/pag2.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sound/palette/palette.dart';
 
 class Pag2 extends StatelessWidget {
   const Pag2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("2", selectionColor: Colors.black,));
+    return const Scaffold(
+      backgroundColor: PALETTE.backgroud,
+      body: Center(child: Text("2", selectionColor: Colors.white)),
+      );
   }
 }
--- lib/screens/pag3.dart
+++ lib/screens/pag3.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sound/palette/palette.dart';
 
 class Pag3 extends StatelessWidget {
   const Pag3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("3", selectionColor: Colors.black,));
+    return const Scaffold(
+      backgroundColor: PALETTE.backgroud,
+      body: Center(child: Text("3", selectionColor: Colors.white)),
+      );
   }
 }
