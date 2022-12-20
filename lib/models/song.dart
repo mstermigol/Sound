53cf52a2dfbe35b00e0e1c4e55f66269d31c5e92
miguel jaramillo <migueljaramillo879@gmail.com>
2022-12-20 14:56:09 -0500

    created Song class

---
+++ lib/models/song.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Song{
+  final String name;
+  final String artist;
+  //final File mp3;
+  final Image image;
+
+  Song(this.name, this.artist, this.image);
+}
